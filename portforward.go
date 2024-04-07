b89520419362928df4970b58e6484954d1bb9e48
Vasantha kumar <d.vasant@gmail.com>
2024-04-07 22:00:17 +0530

    Modified to compile with latest libp2p
    Multiaddress is replaced with peer id and gets resolved through Kad DHT.
    Removed the Deprecated APIs from libp2p

--- portforward.go
+++ portforward.go
@@ -11,19 +11,21 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	// We need to import libp2p's libraries that we use in this project.
 	"github.com/elazarl/goproxy"
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-core/crypto"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
+	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/libp2p/go-libp2p/core/peerstore"
-	"github.com/libp2p/go-tcp-transport"
-	ma "github.com/multiformats/go-multiaddr"
+	"github.com/libp2p/go-libp2p/core/routing"
+	rcmgr "github.com/libp2p/go-libp2p/p2p/host/resource-manager"
+	"github.com/libp2p/go-libp2p/p2p/net/connmgr"
+	"github.com/libp2p/go-libp2p/p2p/security/noise"
+	libp2ptls "github.com/libp2p/go-libp2p/p2p/security/tls"
 	"github.com/phayes/freeport"
 )
 
@@ -35,6 +37,7 @@ type ForwardService struct {
 	host                host.Host
 	localForwardPort    int
 	remoteListeningPort int
+	kadDHT              *dht.IpfsDHT
 }
 
 // Protocol string to identify our application streams
@@ -44,14 +47,21 @@ func NewForwardService(localForwardPort int, remoteListeningPort int, libp2pPort
 
 	// Keeping same peer id always. Create a cert & store it.
 	if _, err := os.Stat("peerid.key"); os.IsNotExist(err) {
-		host, _ := libp2p.New()
+		//host, _ := libp2p.New()
+		priv, _, err := crypto.GenerateKeyPair(
+			crypto.Ed25519, // Select your key type. Ed25519 are nice short
+			-1,             // Select key length when possible (i.e. RSA).
+		)
+		if err != nil {
+			panic(err)
+		}
 		// Get Node's Private Key
-		keyBytes, _ := crypto.MarshalPrivateKey(host.Peerstore().PrivKey(host.ID()))
+		keyBytes, _ := crypto.MarshalPrivateKey(priv)
 		// save to disk
 		f, _ := os.Create("peerid.key")
 		f.WriteString(string(keyBytes))
 		f.Close()
-		host.Close()
+		//host.Close()
 	}
 
 	// Reload the existing keys
@@ -61,57 +71,139 @@ func NewForwardService(localForwardPort int, remoteListeningPort int, libp2pPort
 		log.Fatalln(err)
 	}
 
-	// Only quic for slight advantage over TCP
-	ip4quic := fmt.Sprintf("/ip4/0.0.0.0/udp/%d/quic", libp2pPort)
+	connmgr, err := connmgr.NewConnManager(
+		100, // Lowwater
+		400, // HighWater,
+		connmgr.WithGracePeriod(time.Minute),
+	)
+	if err != nil {
+		panic(err)
+	}
 
+	// Create a resource manager configuration
+	cfg := rcmgr.PartialLimitConfig{
+		System: rcmgr.ResourceLimits{
+			StreamsOutbound: rcmgr.DefaultLimit, // Allow unlimited outbound streams
+			StreamsInbound:  rcmgr.Unlimited,
+			Conns:           4000,
+			ConnsInbound:    500,
+			ConnsOutbound:   500,
+			FD:              10000,
+		},
+	}
+	if len(AcceptedPeers) > 0 {
+		cfg.Peer = make(map[peer.ID]rcmgr.ResourceLimits, 0)
+		for _, acceptedPeer := range AcceptedPeers {
+			id, _ := peer.Decode(acceptedPeer)
+			cfg.Peer[id] = rcmgr.ResourceLimits{
+				// Allow inbound connections from this peer
+				ConnsInbound: rcmgr.Unlimited,
+				// Allow outbound connections to this peer
+				ConnsOutbound: rcmgr.Unlimited,
+			}
+		}
+	}
+	limiter := rcmgr.NewFixedLimiter(cfg.Build(rcmgr.DefaultLimits.AutoScale()))
+	rm, err := rcmgr.NewResourceManager(limiter, rcmgr.WithMetricsDisabled())
+	if err != nil {
+		panic(err)
+	}
+
+	var idht *dht.IpfsDHT
 	host, err := libp2p.New(
-		libp2p.ListenAddrStrings(ip4quic),
+		libp2p.ListenAddrStrings(
+			fmt.Sprintf("/ip4/0.0.0.0/udp/%d/quic-v1", libp2pPort),
+			fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", libp2pPort),
+			//fmt.Sprintf("/ip4/0.0.0.0/tcp/%d/ws", libp2pPort),
+			//fmt.Sprintf("/ip6/::/udp/%d/quic-v1", libp2pPort),
+			//fmt.Sprintf("/ip6/::/tcp/%d", libp2pPort),
+		),
+		libp2p.ResourceManager(rm),
 		libp2p.Identity(privateKey),
-		libp2p.DefaultSecurity,
+		// support TLS connections
+		libp2p.Security(libp2ptls.ID, libp2ptls.New),
+		// support noise connections
+		libp2p.Security(noise.ID, noise.New),
 		libp2p.NATPortMap(),
 		libp2p.DefaultMuxers,
-		libp2p.Transport(libp2pquic.NewTransport),
-		libp2p.Transport(tcp.NewTCPTransport),
+		libp2p.DefaultTransports,
+		libp2p.ConnectionManager(connmgr),
 		libp2p.FallbackDefaults,
+		libp2p.Routing(func(h host.Host) (routing.PeerRouting, error) {
+			idht, err = dht.New(context.Background(), h)
+			return idht, err
+		}),
 	)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	// Set up a channel to receive notifications about identity updates
+	identityChan := make(chan struct{})
+	host.SetStreamHandler("/ipfs/id/push/1.0.0", func(stream network.Stream) {
+		if hasPublicAddress(host) {
+			identityChan <- struct{}{}
+		}
+	})
+
 	// This connects to public bootstrappers
 	for _, addr := range dht.DefaultBootstrapPeers {
 		pi, _ := peer.AddrInfoFromP2pAddr(addr)
-		host.Connect(context.Background(), *pi)
-	}
-
-	var publicAddresses string = ""
-	var privateAddresses string = ""
-	for _, a := range host.Addrs() {
-		if !strings.Contains(a.String(), "127.0.0") &&
-			!strings.Contains(a.String(), "/ip4/10.") &&
-			!strings.Contains(a.String(), "/ip4/172.16.") &&
-			!strings.Contains(a.String(), "/ip4/192.168.") {
-			publicAddresses += fmt.Sprintf("%s/ipfs/%s\n", a, host.ID())
-		} else {
-			privateAddresses += fmt.Sprintf("%s/ipfs/%s\n", a, host.ID())
+		err := host.Connect(context.Background(), *pi)
+		if err != nil {
+			fmt.Println("Boot strap connection failed:", err)
+			continue
 		}
 	}
 
-	fmt.Println("libp2p-peer public addresses:\n", publicAddresses)
-	fmt.Println("libp2p-peer private addresses:\n", privateAddresses)
+	// Wait for identity information to be received
+	select {
+	case <-identityChan:
+		fmt.Println("Identity information received for this host")
+	case <-time.After(30 * time.Second):
+		fmt.Println("Timeout: Identity information not received within 30 seconds")
+	}
+
+	host.RemoveStreamHandler("/ipfs/id/push/1.0.0")
+
+	if hasPublicAddress(host) {
+		fmt.Println("This host proxy server accessible over internet using peer ID :\n", host.ID())
+	} else {
+		fmt.Println("This host  proxy server accessible only over LAN using peer ID:\n", host.ID())
+	}
+	fmt.Println("Add firewall rule to allow : ", libp2pPort, " udp/tcp port")
 
 	return &ForwardService{
 		host:                host,
 		localForwardPort:    localForwardPort,
 		remoteListeningPort: remoteListeningPort,
+		kadDHT:              idht,
 	}
+}
+
+// check if the host has public address
+func hasPublicAddress(host host.Host) bool {
+	for _, a := range host.Addrs() {
+		str := a.String()
+		if !strings.Contains(str, "127.0.0") &&
+			!strings.Contains(str, "/ip4/10.") &&
+			!strings.Contains(str, "/ip4/172.16.") &&
+			!strings.Contains(str, "/ip4/192.168.") {
+			return true
+		}
+	}
+	return false
 }
 
 // Second machine(remote) listens on a proxy port to forward http, https traffic
 // to the first machine over libp2p streams.
 func (f *ForwardService) RemoteListener(destPeer string) {
 
-	destPeerID := addAddrToPeerstore(f.host, destPeer)
+	destPeerID, err := peer.Decode(destPeer)
+	if err != nil {
+		log.Fatal("Destination peer decode failed :", err)
+		return
+	}
 
 	// Setup localListener (type net.Listener)
 	localListener, err := net.Listen("tcp", "0.0.0.0:"+strconv.Itoa(f.remoteListeningPort))
@@ -125,23 +217,26 @@ func (f *ForwardService) RemoteListener(destPeer string) {
 		if err != nil {
 			log.Fatalf("listen.Accept failed: %v", err)
 		}
+
 		stream, err := f.host.NewStream(context.Background(), destPeerID, Protocol)
 		if err != nil {
-			log.Fatalf("Failed to contact remote stream: %v", err)
+			log.Printf("Failed to contact remote stream: %v", err)
+			localConn.Close()
+			continue
 		}
 		// now relay the data between the local socket & stream
 		go func() {
 			defer localConn.Close()
 			_, err = io.Copy(localConn, stream)
 			if err != nil {
-				log.Println("Local connection to stream failed: %v", err)
+				log.Println("Local connection to stream failed:", err)
 			}
 		}()
 		go func() {
 			defer stream.Close()
 			_, err = io.Copy(stream, localConn)
 			if err != nil {
-				log.Println("Stream to local connection failed: %v", err)
+				log.Println("Stream to local connection failed:", err)
 			}
 		}()
 	}
@@ -154,14 +249,14 @@ func (f *ForwardService) localStreamForwarder(stream network.Stream) {
 	if len(AcceptedPeers) > 0 {
 		var accepted bool = false
 		for _, peer := range AcceptedPeers {
-			if peer == stream.Conn().RemotePeer().Pretty() {
+			if peer == stream.Conn().RemotePeer().String() {
 				accepted = true
-				fmt.Println("Accepting connection from first peer", stream.Conn().RemotePeer().Pretty())
+				fmt.Println("Accepting connection from first peer", stream.Conn().RemotePeer().String())
 			}
 		}
 
 		if !accepted {
-			fmt.Println("Blocking connection from peer", stream.Conn().RemotePeer().Pretty())
+			fmt.Println("Blocking connection from peer", stream.Conn().RemotePeer().String())
 			// silently drop the connection
 			stream.Close()
 			return
@@ -170,55 +265,25 @@ func (f *ForwardService) localStreamForwarder(stream network.Stream) {
 
 	target, err := net.Dial("tcp", "127.0.0.1:"+strconv.Itoa(f.localForwardPort))
 	if err != nil {
-		log.Fatal("could not connect to target", err)
+		log.Println("could not connect to target", err)
 		stream.Close()
+		return
 	}
 	// Relay data between the stream & socket
 	go func() {
 		defer target.Close()
 		_, err = io.Copy(target, stream)
 		if err != nil {
-			log.Println("Stream connection closed with proxy failed: %v", err)
+			log.Println("Stream connection closed with proxy failed:", err)
 		}
 	}()
 	go func() {
 		defer stream.Close()
 		_, err = io.Copy(stream, target)
 		if err != nil {
-			log.Println("Proxy connection closed with stream failed: %v", err)
+			log.Println("Proxy connection closed with stream failed: ", err)
 		}
 	}()
-}
-
-// addAddrToPeerstore parses a peer multiaddress and adds
-// it to the given host's peerstore, so it knows how to
-// contact it. It returns the peer ID of the remote peer.
-func addAddrToPeerstore(h host.Host, addr string) peer.ID {
-	// The following code extracts target's the peer ID from the
-	// given multiaddress
-	ipfsaddr, err := ma.NewMultiaddr(addr)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	pid, err := ipfsaddr.ValueForProtocol(ma.P_IPFS)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	peerid, err := peer.Decode(pid)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// Decapsulate the /ipfs/<peerID> part from the target
-	// /ip4/<a.b.c.d>/ipfs/<peer> becomes /ip4/<a.b.c.d>
-	targetPeerAddr, _ := ma.NewMultiaddr(fmt.Sprintf("/ipfs/%s", peerid))
-	targetAddr := ipfsaddr.Decapsulate(targetPeerAddr)
-
-	// We have a peer ID and a targetAddr so we add
-	// it to the peerstore so LibP2P knows how to contact it
-	h.Peerstore().AddAddr(peerid, targetAddr, peerstore.PermanentAddrTTL)
-	return peerid
 }
 
 const help = `
@@ -241,9 +306,9 @@ func main() {
 	}
 
 	// Parse command line flags
-	destPeer := flag.String("d", "", "destination peer address")
+	destPeer := flag.String("d", "", "destination peer address sharing internet over proxy server")
 	proxyPort := flag.Int("p", 8080, "proxy port")
-	p2pport := flag.Int("l", 12000, "libp2p listen port")
+	p2pport := flag.Int("l", 12007, "libp2p listen port")
 	acceptedPeerList := flag.String("a", "", "Accepted peer IDs")
 	printOnly := flag.Bool("i", false, "Print peer ID information")
 
